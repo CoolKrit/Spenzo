b744f1e7df602de16730afc0116456c7bfd0345d
Amir Makhmudov <amir-makhmudov2004@mail.ru>
2024-04-30 10:54:34 +0300

    Fragment layouts have been created (SignInFragment, SignUpFragment)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -42,6 +45,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    // Navigation
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    // Fragment
+    implementation(libs.androidx.fragment.ktx)
+    // SplashScreen
+    implementation(libs.androidx.core.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
