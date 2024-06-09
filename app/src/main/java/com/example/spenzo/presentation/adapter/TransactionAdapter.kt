0ad3a18c6e846ed9c983473b991058a581d82fa7
Amir Makhmudov <amir-makhmudov2004@mail.ru>
2024-06-10 00:04:47 +0300

    Added Icons to Transactions

--- app/src/main/java/com/example/spenzo/presentation/adapter/TransactionAdapter.kt
+++ app/src/main/java/com/example/spenzo/presentation/adapter/TransactionAdapter.kt
@@ -19,10 +19,12 @@ import com.example.spenzo.presentation.activity.MainActivity.Companion.SETTINGS
 import java.text.NumberFormat
 import java.util.Locale
 
-class TransactionAdapter(private val onClick: (Transaction) -> Unit) : ListAdapter<Transaction, TransactionAdapter.ItemViewHolder>(TransactionDiffCallback()) {
+class TransactionAdapter(private val onClick: (Transaction) -> Unit) :
+    ListAdapter<Transaction, TransactionAdapter.ItemViewHolder>(TransactionDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val binding = ItemTransactionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ItemTransactionBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ItemViewHolder(binding)
     }
 
@@ -34,7 +36,8 @@ class TransactionAdapter(private val onClick: (Transaction) -> Unit) : ListAdapt
         }
     }
 
-    inner class ItemViewHolder(private val binding: ItemTransactionBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ItemViewHolder(private val binding: ItemTransactionBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(item: Transaction) {
             binding.transactionTitle.text = item.title
             binding.transactionCategory.text = item.category
@@ -54,9 +57,61 @@ class TransactionAdapter(private val onClick: (Transaction) -> Unit) : ListAdapt
                 else -> com.google.android.material.R.attr.colorOnSurface
             }
 
-            binding.transactionAmount.setTextColor(ContextCompat.getColor(binding.transactionAmount.context, colorRes))
+            when (item.category) {
+                "Housing" -> {
+                    binding.transactionIconView.setImageResource(R.drawable.ic_food)
+                }
+
+                "Transportation" -> {
+                    binding.transactionIconView.setImageResource(R.drawable.ic_transport)
+                }
+
+                "Food" -> {
+                    binding.transactionIconView.setImageResource(R.drawable.ic_food)
+                }
+
+                "Utilities" -> {
+                    binding.transactionIconView.setImageResource(R.drawable.ic_utilities)
+                }
+
+                "Insurance" -> {
+                    binding.transactionIconView.setImageResource(R.drawable.ic_insurance)
+                }
+
+                "Healthcare" -> {
+                    binding.transactionIconView.setImageResource(R.drawable.ic_medical)
+                }
+
+                "Saving & Debts" -> {
+                    binding.transactionIconView.setImageResource(R.drawable.ic_savings)
+                }
+
+                "Personal Spending" -> {
+                    binding.transactionIconView.setImageResource(R.drawable.ic_personal_spending)
+                }
+
+                "Entertainment" -> {
+                    binding.transactionIconView.setImageResource(R.drawable.ic_entertainment)
+                }
+
+                "Miscellaneous" -> {
+                    binding.transactionIconView.setImageResource(R.drawable.ic_other)
+                }
+
+                else -> {
+                    binding.transactionIconView.setImageResource(R.drawable.ic_other)
+                }
+            }
+
+            binding.transactionAmount.setTextColor(
+                ContextCompat.getColor(
+                    binding.transactionAmount.context,
+                    colorRes
+                )
+            )
             val formattedAmount = formatNumber(item.amount)
-            val amountText = if (item.type == "Income") "+$currency${formattedAmount}" else "-$currency${formattedAmount}"
+            val amountText =
+                if (item.type == "Income") "+$currency${formattedAmount}" else "-$currency${formattedAmount}"
             binding.transactionAmount.text = amountText
         }
 
